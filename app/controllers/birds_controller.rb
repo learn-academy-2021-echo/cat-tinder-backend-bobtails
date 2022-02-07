631735e8f6c97d9e28fc5b3eda058f9fdb112d9b
kickeyhickey <irishmanhickey@gmail.com>
2022-02-07 09:42:07 -0800

    finished

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,2 +1,24 @@
 class BirdsController < ApplicationController
+    def index
+      birds = Bird.all
+      render json: birds
+    end
+
+    def create
+      bird = Bird.create(bird_params)
+      render json: bird
+    end
+
+    def update
+      
+    end
+
+    def destroy
+    end
+
+  private
+  def bird_params
+    params.require(:bird).permit(:name, :age, :enjoys, :image)
+  end
+
 end
